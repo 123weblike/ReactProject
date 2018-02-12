23c5a7a7a85b964f908807a8f3ca456578b1af37
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-02-12 09:31:11 +0800

    React

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import {render} from 'react-dom';
-render(<h1>hello</h1>,document.getElementById('root'));
+import App from "./containers/index";
+render(<App/>,document.getElementById('root'));
