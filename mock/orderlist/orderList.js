e6b0812d7f34ea33228e6593fff1237d025c5560
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-02-18 22:38:20 +0800

    模拟的后台数据

---
+++ mock/orderlist/orderList.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: Date.now(),
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518937937331&di=db42bdb51aec898771fcb6fa1b09ea04&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F22%2F95%2F97%2F57f027e03ebab_1024.jpg',
+        title: '汉堡大王',
+        count: 3,
+        price: '167',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518937990164&di=7da0d5a55abc1f57df2f22346db9bb9b&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fbainuo%2Fwh%3D720%2C436%2Fsign%3Db1e20cfa96ef76c6d087f32caf26d1ce%2F7acb0a46f21fbe093f5c1dcf6d600c338644adef.jpg',
+        title: '麻辣香锅',
+        count: 1,
+        price: '188',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518938031312&di=b611ce48602ae29f1d2c3ec931ce925e&imgtype=0&src=http%3A%2F%2Fimage.liuxue360.com%2F2014%2F12%2F01%2F20141201115131859.jpg',
+        title: '好吃自出餐',
+        count: 2,
+        price: '110',
+        commentState: 2
+    }
+]
