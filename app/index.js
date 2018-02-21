cf4c12da5d4a9ebbc3b85e3b7c4c50635875ffc4
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-02-21 20:34:08 +0800

    index

--- app/index.js
+++ app/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+//App负责选择哪一个页面
+import './assets/index.less';
 import App from "./containers/index";
-render(<App/>,document.getElementById('root'));
+import {Provider} from 'react-redux';  //注入store
+//导入store
+import {configureStore} from './store';
+//可以在configureStore传入初始状态
+let store = configureStore();//生成store
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,document.getElementById('root'));
