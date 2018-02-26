d3b17fbc84d2aed623f25d3cfb95acf829c1e66f
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-02-26 09:34:15 +0800

    index

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,6 +1,12 @@
 import React,{Component} from 'react';
 import RouterMap from "../routes/index";
-export default class App extends Component{
+import {connect} from 'react-redux';
+//绑定actionCreator组成的对象
+import {bindActionCreators} from 'redux';
+//获取所有actionCreator组成的对象
+import * as Actions from '../actions/userInfo';
+import {getStorage} from '../local'
+class App extends Component{
     constructor(){
         super();
         this.state={
@@ -16,8 +22,32 @@ export default class App extends Component{
        )
     }
     componentDidMount(){
+        //先去本地查找 是否存储过localstorage名字叫cityName的
+        //1.没有cityName ，赋予一个默认值 杭州
+        //2.将当前的城市存放到redux
+        let cityName = getStorage('cityName');
+        if(cityName == null){
+            cityName = '杭州'
+        }
+        //页面加载后，设置一个城市
+        this.props.userActions.update({
+            cityName
+        });
         this.setState({
             done:true
         });
+
     }
 }
+//export default 为默认导出
+export default connect(
+    state=>{       //mapStateToProps  获取redux内容
+        return {}
+    },
+    dispatch=>{ //mapDispatchToProps  修改redux属性
+        return {
+            //dispatch(action,data);
+            userActions:bindActionCreators(Actions,dispatch)
+        }
+    }
+)(App)//将状态和动作以属性的形式传递给了App
