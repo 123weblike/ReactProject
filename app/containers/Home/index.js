5b0f3750fef8ab9af5542f8e510aa6f117526b03
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-03-07 22:37:55 +0800

    Home

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,10 +1,24 @@
 import React,{Component} from 'react';
-export default class Home extends Component{
+import HomeHeader from "../../components/HomeHeader/index";
+import Slider from "../../components/slider/index";
+import Ad from "./subpage/Ad";
+import {connect} from 'react-redux';
+import List from './subpage/List';
+class Home extends Component{
     render(){
        return (
            <div>
-               Home
+               <HomeHeader cityName={this.props.userInfo.cityName} history={this.props.history}/>
+               <Slider/>
+               <Ad/>
+               <List cityName={this.props.userInfo.cityName}/>
            </div>
        )
     }
 }
+//取出redux中的城市传递给HomeHeader组件
+export default connect(
+    state=>{
+        return {userInfo:state.userInfo} //取出redux state中userInfo的数据，赋予给变量userInfo
+    }
+)(Home);
